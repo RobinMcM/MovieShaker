c3a1fdae60c00b4707054173332d775dfadbbe71
Robin McManus <robin@dcs.uk.net>
2024-06-27 12:50:59 +0100

    The first commit

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { Form, Input, Button } from 'antd';
+import Password from 'antd/es/input/Password';
+
+function Login() {
+  const onFinish = (values) => {
+    console.log('sucess', values);
+  };
+  return (
+    <div className="grid grid-cols-2">
+      <div className="bg-primary h-screen flex flex-col justify-center items-center">
+        <div>
+          <h1 className="text-6xl text-white"> MovieShaker Ltd</h1>
+          <span className="text-white">This is where your dream starts</span>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="w-[300px]">
+          <h1>Login</h1>
+
+          <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item label="email" name="email">
+              <Input />
+            </Form.Item>
+            <Form.Item label="password" name="password">
+              <Input.Password />
+            </Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Submit
+            </Button>
+            <div className="flex justify-center mt-5">
+              <span>
+                need an account <Link to="/register"> Register</Link>
+              </span>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
